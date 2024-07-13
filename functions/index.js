c37c2045c09afb1c306352a396aed0afbc84f175
yuni <hys990730@gmail.com>
2024-07-13 21:59:30 +0900

    chore: Sharp 패키지 가져오기

--- functions/index.js
+++ functions/index.js
@@ -1,18 +1,3 @@
 const { onRequest } = require("firebase-functions/v2/https");
-const { GoogleGenerativeAI } = require("@google/generative-ai");
-
-const genAI = new GoogleGenerativeAI(process.env.API_KEY);
-
-exports.sayHello = onRequest(
-    { cors: true },
-    async (req, res) => {
-      const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
-
-      const prompt = "Write a story about a magic backpack.";
-
-      const result = await model.generateContent(prompt);
-      const response = await result.response;
-      const text = await response.text();
-      res.status(200).send(text);
-    },
-);
+const { GoogleGenerativeAI, HarmBlockThreshold, HarmCategory } = require("@google/generative-ai");
+const sharp = require('sharp'); // Import sharp for image processing
