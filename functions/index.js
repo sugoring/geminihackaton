e669f8d4a44c12a112f49f2bf6627cee7aa873ad
yuni <hys990730@gmail.com>
2024-07-13 21:50:23 +0900

    feat: Generative Model 가져오기 및 프롬프트 설정

--- functions/index.js
+++ functions/index.js
@@ -6,6 +6,15 @@ const genAI = new GoogleGenerativeAI(process.env.API_KEY);
 exports.sayHello = onRequest(
     { cors: true },
     async (req, res) => {
-    // 나머지 코드 여기에 추가
+      exports.sayHello = onRequest(
+          { cors: true },
+          async (req, res) => {
+            const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+            const prompt = "Write a story about a magic backpack.";
+
+          // 나머지 코드 여기에 추가
+          },
+      );
     },
 );
