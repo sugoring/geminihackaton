286a0cb82861011b9162160075082c7aaa58faa6
yuni <hys990730@gmail.com>
2024-07-13 22:00:05 +0900

    feat: Firebase Function 정의 및 CORS 설정

--- functions/index.js
+++ functions/index.js
@@ -1,3 +1,7 @@
 const { onRequest } = require("firebase-functions/v2/https");
 const { GoogleGenerativeAI, HarmBlockThreshold, HarmCategory } = require("@google/generative-ai");
 const sharp = require('sharp'); // Import sharp for image processing
+const genAI = new GoogleGenerativeAI(process.env.API_KEY);
+exports.analyzeImage = onRequest({ cors: true }, async (req, res) => {
+  // 나머지 코드 여기에 추가
+});
