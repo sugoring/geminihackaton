7593a2fdca24690dd599e6816dfc1f03a2b4d472
yuni <hys990730@gmail.com>
2024-07-13 21:49:55 +0900

    feat: GoogleGenerativeAI 인스턴스 생성

--- functions/index.js
+++ functions/index.js
@@ -1,2 +1,4 @@
 const { onRequest } = require("firebase-functions/v2/https");
 const { GoogleGenerativeAI } = require("@google/generative-ai");
+
+const genAI = new GoogleGenerativeAI(process.env.API_KEY);
